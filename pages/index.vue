0b3c0a4da4669f91878802d88d80a489aef30965
Diego M. Rodrigues <diegomr86@gmail.com>
2020-06-16 11:00:19 -0300

    Big bang

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="text-center mt-4">
+      <b-button variant="primary">Adicionar postagem</b-button>
+    </div>
+    <h1 class="title">
+      Últimas postagens
+    </h1>
+    <pre>{{ posts }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: null
+    }
+  },
+  async created() {
+    this.posts = await this.$axios.$get('posts')
+    console.log(this.image())
+  }
+}
+</script>
